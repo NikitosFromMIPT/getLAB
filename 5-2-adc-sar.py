69e5c2b9c393db3389fe34c92630136e32f4d02b
NikitosFromMIPT <kuznetsov.nv@phystech.edu>
2024-10-01 12:44:57 +0300

    скрипт реализующий работу АЦП в котором перебор оптимизирован

--- 5-2-adc-sar.py
+++ 5-2-adc-sar.py
@@ -2,9 +2,12 @@ import RPi.GPIO as gpio
 import time
 
 gpio.setmode(gpio.BCM)
+
 dac=[8, 11, 7, 1, 0, 5, 12, 6]
+led=[2, 3, 4, 17, 27, 22, 10, 9]
 comp = 14
 troyka = 13
+
 gpio.setup(dac, gpio.OUT)
 gpio.setup(troyka, gpio.OUT, initial=gpio.HIGH)
 gpio.setup(comp, gpio.IN)
@@ -14,17 +17,21 @@ def dec2bin(n):
 
 
 def adc():
-    for i in range(2^8):
+    n = 0
+    for i in range(256):
         gpio.output(dac, dec2bin(i)) # в модуле DAC выводим ддвоичную запись числа i
         time.sleep(0.01)
-        if gpio.input(comp) == 1: # пин COMP выход компаратора, если он считывает 0 то cигнал на DAC больше сигнала на тройке модуле
-            return(i)
+        if gpio.input(comp) == 1: # пин COMP выход компаратора, если он считывает 0 то выводит число i
+            n = i
+            break
+    return(n)
 
 try:
     while True:
         Vn =adc()
-        if Vn!=None:
-            print(Vn)
+        if Vn!=0:
+            print(Vn*3.6/256,"В")
+           
 
 finally:
     gpio.output(dac, 0)
